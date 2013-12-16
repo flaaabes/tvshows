bf9f5adb9e0ed70491752610dc1fa367bda3f71d
ralmoe <ralf.moedder@finocom.de>
2013-12-16 11:42:25 +0100

    poster download job

--- jobs/fetch_posters.php
+++ jobs/fetch_posters.php
@@ -17,12 +17,12 @@
     $img_basedir = $options->get('banner_directory');
     $tvdb_url    = $options->get('tvdb_url');
     
-    $db_images = $db->load_all_objects('tv_shows',array('banner'));
+    $db_images = $db->load_all_objects('tv_shows',array('poster'));
     
     foreach($db_images as $image){
-        $image_path = $img_basedir.$image['banner'];
+        $image_path = $img_basedir.$image['poster'];
         if(!file_exists($image_path)){
-            $img_url = $tvdb_url.'/banners/_cache/'.$image['banner'];
+            $img_url = $tvdb_url.'/banners/_cache/'.$image['poster'];
             echo $img_url."\n";
             file_put_contents($image_path, fopen($img_url, 'r'));
         }
